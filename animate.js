215ace1130f074cc4f94f3b119593259ced47227
chantal2000 <nniyonkuruchantal@gmail.com>
2021-06-28 19:42:54 +0300

    E-Commerce

---
+++ animate.js
@@ -0,0 +1,13 @@
+const text = document.querySelector(".fancy")
+
+function() {
+
+}
+// const strText = text.textContent;
+// const splitText = strText.split("");
+// text.textContent = ""
+// for (let i = 0; i <= splitText.length; i++) {
+//     text.innerHTML = "<span>" + splitText[i]
+//     "</span>"
+
+// }
---
+++ index.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+function check() {
+    var dots = document.getElementById("dot");
+    var moreText = document.getElementById("more")
+    var btnText = document.getElementById("btn")
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none"
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Readless"
+        moreText.style.display = "inline"
+    }
+}
+
+
+if (text === "") {
+    document.getElementById("id").innerHTML = "Please This can't be empty!"
+}
